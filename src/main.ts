09d5a30fecf31f3a091bf6aed43518b53670824d
Michaela <michaelachan45@gmail.com>
2024-03-11 16:32:58 +0000

    removed comments from main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,69 +1,4 @@
-//togglemode for different themes.
 
-// need user to press on buttons
-// const multiplyNum = (num1: number, num2: number): number => {
-//   return num1 * num2;
-// };
-
-// console.log(multiplyNum(2, 2));
-// const result = multiplyNum(2,2) // storing the result
-//store is in my ANS button.
-
-// const clickButton = document.querySelectorAll<HTMLButtonElement>(".article__button");
-// clickButton.forEach(button => {
-//   button.addEventListener("click", )
-// })
-
-//have to handle user clicks
-//event listeners
-//functions for operations
-
-//This allows for multiple inputs.
-// const addNum= (numArr: number[])=> {
-//   let total = 0;
-//   for (let index = 0; index < numArr.length; index++) {
-//     total += numArr[index];
-//   }
-//   return total;
-// }
-
-// let nums = [3,5,6,4]
-// const sum = addNum(nums);
-// console.log(sum)
-
-//need an if else? to do multiple operations in one?
-// const calculate = (numArr: number[]) => {
-//   let total = 0;
-//   for (let i = 0; i < numArr.length; i++) {
-//     if ()
-
-//   }
-// }
-
-// const handleClickButton = (event: Event) => {
-//     const clickedButton = event.target as HTMLButtonElement;
-//     const buttonValue = clickedButton.textContent;
-
-//     if(buttonValue === "="){
-//         result.value = eval(result.value) // change this
-//     } else if (buttonValue === "C"){
-//         result.value = " "
-//     } else if (buttonValue === "DEL"){
-//         result.value = result.value.slice(0,-1)
-//     } else {
-//         result.value += buttonValue;
-//     }
-// }
-// buttons.forEach((button) => {
-//     button.addEventListener("click", handleClickButton);
-//   });
-
-//   const calculate = (event:Event[]) => {
-//     if(buttonNumValue === "+")
-//   }
-
-//change classes
-//store num 1, operators, num 2,
 const darkMode = document.querySelector<HTMLButtonElement>(
   ".aside__button--dark"
 );
@@ -185,24 +120,6 @@ const handleDecimalButton = (event: Event) => {
 };
 decimalButton.addEventListener("click", handleDecimalButton);
 
-// const calculateResult = (num1: number, num2: number, op: string)=> {
-//   switch(op) {
-//     case "+":
-//       return num1 + num2;
-//     case "-":
-//       return num1 - num2;
-//     case "x":
-//       return num1 * num2;
-//     case "÷":
-//       if (num2 === 0) {
-//         return NaN;
-//       } else {
-//         return num1 / num2;
-//       }
-//       default:
-//         return NaN;
-//   }
-// }
 
 const handleEqualButton = () => {
   const expression = result.value;
